f2a9d7ae2ec7d9732535fc167c0bc089f65cc3ca
Oleg Mostepan <peroman86@gmail.com>
2025-01-04 11:30:40 +0200

    Lower cooldown time for ammo pile

--- scripts/ammo_pile.gd
+++ scripts/ammo_pile.gd
@@ -4,14 +4,14 @@ extends Area2D
 var player_in_range = false
 var current_player = null
 var can_collect = true
-var cooldown_time = 0.5  # seconds
+var cooldown_time = 0.1  # reduced cooldown time
 
 func _ready():
 	connect("body_entered", _on_body_entered)
 	connect("body_exited", _on_body_exited)
 
 func _process(_delta):
-	if player_in_range and Input.is_key_pressed(KEY_E) and can_collect:
+	if player_in_range and Input.is_action_just_pressed("collect") and can_collect:
 		if current_player.has_method("collect_ammo"):
 			current_player.collect_ammo(ammo_amount)
 			can_collect = false
